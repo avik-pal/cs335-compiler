e0a92f74ba233c6d384674c097fac9755d35eae8
purug2000 <puru.g2000@gmail.com>
2021-02-06 22:45:28 +0530

    [Buggy] Modified accum.c, added lex and yacc files

--- src/lexer/accum.c
+++ src/lexer/accum.c
@@ -2,35 +2,18 @@
 #include "../header/token.h"
 #include "../header/utils.h"
 #include <stdio.h>
-
+#include <string.h>
+#include "y.tab.h"
 // Allocate more than being used by the vector.
 #define CVECTOR_LOGARITHMIC_GROWTH
 
 int counter = 0;
 
-int get_token_info(int *token_number, char **token, char **lexeme, int *line_number, int *column_number) {
-    *token_number = counter;
-    if(counter % 2 == 0) {
-        *token = "ADWD";
-        *lexeme = "sadsd";
-    }
-    else {
-        *token = "2A12DWD";
-        *lexeme = "sad1212sd";
-    }
-    *line_number = counter * 3;
-    *column_number = counter * 9;
-
-    ++counter;
-
-    // Returning 0 means no token
-    if (counter == 2)
-        return 0;
-    else
-        return 1;
-}
-
-
+extern int line;
+extern int column;
+extern char* token_name();
+extern char yytext[];
+extern int yylex();
 /*
     Get the list of TokenInfo by querying the parser/scanner
 */
@@ -40,20 +23,16 @@ cvector_vector_type(TokenInfo) accumulate() {
     char *token_orig, *lexeme_orig, *token, *lexeme;
     int token_number, line_number, column_number;
 
-    int more_tokens;
-
-    while(1)
-    {
-        more_tokens = get_token_info(&token_number, &token_orig, &lexeme_orig, &line_number, &column_number);
-
-        if(more_tokens == 0) {
-            break;
-        }
-
+    int flag = 1;
+    while ( (token_number = yylex()) > 0 ) {
+        // printf("%d %s",token_number,token_name(token_number));
+        // yytext[strlen(yytext)-2] = '\0';
+        printf("%s", yytext );
         // Copy the TOKEN and LEXEMEs for safety
-        token = strcpy_alloc(token_orig);
-        lexeme = strcpy_alloc(lexeme_orig);
-            
+        token = strcpy_alloc(token_name(token_number));
+        lexeme = strcpy_alloc(yytext);
+        line_number = line;
+        column_number = column;
         // Create a new struct and append to the vector
         TokenInfo tkn_info = {token_number, token, lexeme, line_number, column_number};
         cvector_push_back(vec, tkn_info);
@@ -63,6 +42,16 @@ cvector_vector_type(TokenInfo) accumulate() {
 }
 
 int main(int argc, char **argv) {
+    extern FILE *yyin, *yyout; 
+  
+    /* yyin points to the file input.txt 
+    and opens it in read mode*/
+    yyin = fopen(argv[1], "r"); 
+    printf("%s",argv[1]);
+    /* yyout points to the file output.txt 
+    and opens it in write mode*/
+    // yyout = fopen("Output.txt", "w"); 
+    printf("here");
     cvector_vector_type(TokenInfo) vec = accumulate();
     if (vec) {
         TokenInfo *it;
