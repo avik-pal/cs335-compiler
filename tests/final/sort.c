28db280a51455ea88c4e9573d47383cac539deaf
yasharthb <yasharthbajpai.99@gmail.com>
2021-05-13 22:18:09 +0530

    Dynamic memory in Test Case

--- tests/final/sort.c
+++ tests/final/sort.c
@@ -1,12 +1,15 @@
 @include "stdlib/io/console_output.c"
 @include "stdlib/io/console_input.c"
+@include "stdlib/mem/malloc.c"
 
 int main(){
     int t,n;
     int i,k,j;
-    float arr[100];
+    float* arr;
     
-    n = read_float();
+    n = read_int();
+    arr = malloc(sizeof(float)*n);
+
     for(i=0;i<n;i++){
         arr[i] = read_float();
     }
